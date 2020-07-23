5507e53a3cf98bd246d7661df7fa31976c33c1a9
Randel Jiao <randeljiao@gmail.com>
2020-07-23 19:53:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,12 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+x_win_diagonal = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+winner(x_win_diagonal) #=> "X"
+ 
+o_win_center_column = ["X", "O", " ", " ", "O", " ", " ", "O", "X"]
+winner(o_win_center_column) #=> "O"
+ 
+no_winner_board = ["X", "O", " ", " ", " ", " ", " ", "O", "X"]
+winner(no_winner_board) #=> nil
